272339900e69ca24c8eb8543ec61abfdbd42d297
OoiYE <88772086+OoiYE@users.noreply.github.com>
2024-06-28 09:53:01 +0800

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int num1, num2;
+	cout << "Please enter first number: ";
+	cin >> num1;
+	cout << "Please enter second number: ";
+	cin >> num2;
+
+	int products;
+	products = num1 * num2;
+	cout << "The products of the numbers is: " << products;
+	return 0;
+}
