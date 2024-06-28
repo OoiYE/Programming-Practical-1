040ea7d068c5a117e70082da05f455642cd336fa
OoiYE <88772086+OoiYE@users.noreply.github.com>
2024-06-28 09:49:39 +0800

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int num;
+	cout << "Please enter a number: ";
+	cin >> num;
+
+	int square;
+	square = num * num;
+	cout << "The square of the number is: " << square;
+	return 0;
+}
